45718de318581334f51f3206565b920a7b392ce0
Andrew Kalek <andrew.kalek@anlek.com>
2013-07-08 11:00:57 -0700

    Removed easyXDM in favor of using ajax cross domain

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -8,6 +8,7 @@ files = [
   'app/js/BWL/base.js.coffee',
   'app/lib/ender/ender.js',
   'app/lib/i18next/release/i18next-1.6.3.js',
+  'app/js/BWL/prerequisite.js.coffee',
   'app/js/BWL/data_access.js.coffee',
   'app/js/**/*.js.coffee',
   'test/unit/**/*.js.coffee'
