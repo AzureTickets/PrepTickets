b7a0b870c5f1cd59768317d34942f76d0c03b211
Andrew Kalek <andrew.kalek@anlek.com>
2013-08-08 17:29:30 -0700

    Another fix for stylesheet tag being messed up.

--- lib/helpers/my_tag_helper.rb
+++ lib/helpers/my_tag_helper.rb
@@ -1,12 +1,11 @@
-require 'active_support/core_ext/hash/reverse_merge'
 module MyTagHelper
   extend ActiveSupport::Concern  
   
   def stylesheet_tag(source, options={})
-    tag("link", { "rel" => "stylesheet", "type" => "text/css", "media" => "screen", "href" => source }.reverse_merge(options), false)
+    tag("link", { "rel" => "stylesheet", "type" => "text/css", "media" => "screen", "href" => source }.merge(options.stringify_keys), false)
   end
   def javascript_tag(source, options={})
-    content_tag("script", "", { "type" => "text/javascript", "src" => source }.reverse_merge(options))
+    content_tag("script", "", { "type" => "text/javascript", "src" => source }.merge(options.stringify_keys))
   end
 
   def stylesheet_link_tag name, options={}
