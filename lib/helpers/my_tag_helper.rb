c36b749efdfda3f907f0b6b73b12cc3f0c69c25d
Andrew Kalek <andrew.kalek@anlek.com>
2013-08-08 17:20:38 -0700

    Fixed IE8 javascript loading issue
    Figured out why IE8 and below weren't loading the content correctly.
    
    Fixed issue where CSS link would have double media tags

--- lib/helpers/my_tag_helper.rb
+++ lib/helpers/my_tag_helper.rb
@@ -1,11 +1,12 @@
+require 'active_support/core_ext/hash/reverse_merge'
 module MyTagHelper
   extend ActiveSupport::Concern  
   
   def stylesheet_tag(source, options={})
-    tag("link", { "rel" => "stylesheet", "type" => "text/css", "media" => "screen", "href" => source }.merge(options), false)
+    tag("link", { "rel" => "stylesheet", "type" => "text/css", "media" => "screen", "href" => source }.reverse_merge(options), false)
   end
   def javascript_tag(source, options={})
-    content_tag("script", "", { "type" => "application/javascript", "src" => source }.merge(options))
+    content_tag("script", "", { "type" => "text/javascript", "src" => source }.reverse_merge(options))
   end
 
   def stylesheet_link_tag name, options={}
